684c794b648c16b53c8c879dfba91a447c2dae51
Shane <reiko@reikooters.net>
2013-10-06 18:11:30 +1100

    Progress

--- src/net/reikooters/Jotter/MainActivity.java
+++ src/net/reikooters/Jotter/MainActivity.java
@@ -1,13 +1,21 @@
 package net.reikooters.Jotter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
 
 import java.util.ArrayList;
@@ -18,6 +26,7 @@ import java.util.List;
 public class MainActivity extends Activity
 {
     public final static String EXTRA_MESSAGE = "net.reikooters.Jotter.MESSAGE";
+    ViewAnimator viewAnimator;
 
     /**
      * Called when the activity is first created.
@@ -28,7 +37,17 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        viewAnimator = (ViewAnimator) findViewById(R.id.viewAnimator);
+
+        final Animation inAnim = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        final Animation outAnim = AnimationUtils.loadAnimation(this,android.R.anim.slide_out_right);
+
+        viewAnimator.setInAnimation(inAnim);
+        viewAnimator.setOutAnimation(outAnim);
+
         final ListView listview = (ListView) findViewById(R.id.listView);
+        final ListView listview2 = (ListView) findViewById(R.id.listView2);
+
         String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
                 "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
                 "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
@@ -42,6 +61,7 @@ public class MainActivity extends Activity
         final StableArrayAdapter adapter = new StableArrayAdapter(this,
                 android.R.layout.simple_list_item_1, list);
         listview.setAdapter(adapter);
+        listview2.setAdapter(adapter);
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
@@ -116,14 +136,67 @@ public class MainActivity extends Activity
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle presses on the action bar items
         switch (item.getItemId()) {
-            case R.id.action_search:
-                //openSearch();
-                return true;
+            //case R.id.action_search:
+            //    openSearch();
+            //    return true;
             case R.id.action_settings:
                 //openSettings();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    /*
+    @Override
+    public void onBackPressed()
+    {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+    }
+    */
+
+    void openSearch()
+    {
+        //DialogFragment newFragment = new SearchDialogFragment();
+        //newFragment.show(null, "search");
+
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //builder.setMessage(R.string.dialog_search_message);
+        builder.setTitle(R.string.dialog_search_title);
+
+        final EditText searchText = new EditText(this);
+        searchText.setEllipsize(TextUtils.TruncateAt.END);
+        searchText.setInputType(InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE);
+        builder.setView(searchText);
+
+        // Add the buttons
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int id)
+            {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int id)
+            {
+                // User cancelled the dialog
+            }
+        });
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+
+        searchText.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(searchText, InputMethodManager.SHOW_FORCED);
     }
 }
